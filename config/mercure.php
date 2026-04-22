727ee0a975ab8d667281404645aaa5b9ed6f62f1
matheusbren <matheusbren@alunos.utfpr.edu.br>
2026-04-22 10:39:39 -0300

    fix: remove weak default for MERCURE_JWT_SECRET

--- config/mercure.php
+++ config/mercure.php
@@ -2,5 +2,5 @@
 
 return [
     'url' => env('MERCURE_URL', 'http://localhost/.well-known/mercure'),
-    'jwt_secret' => env('MERCURE_JWT_SECRET', ''),
+    'jwt_secret' => env('MERCURE_JWT_SECRET'),
 ];
